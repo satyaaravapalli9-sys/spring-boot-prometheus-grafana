ce5c27c27f1469fe593ee1ea9ffb1379d2a94def
satyaaravapalli9-sys <satyaaravapalli9@gmail.com>
2025-04-06 18:30:00 +0000

    feat: enhance SpringBootPrometheusGrafanaApplicationTests helper methods

---
+++ src/test/java/com/hendisantika/springbootprometheusgrafana/SpringBootPrometheusGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootprometheusgrafana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootPrometheusGrafanaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
